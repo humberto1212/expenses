dc0d43314eb12dc4fb3fbabbf5effd9027b509fe
Humberto <angeluccirincon@gmail.com>
2022-01-19 17:09:00 +0100

    delete variable and condition

--- src/components/expenses/ExpensesItemList.js
+++ src/components/expenses/ExpensesItemList.js
@@ -24,36 +24,17 @@ function ExpensesItemList(props) {
         }
     } 
 
-    //the filtered data is save in this variable
-    let filteredData =  filteredDataPerYear(props.onExpensesData).map((expenses)=> {
-        return(
-            <ExpenseItem  
-                key={expenses.id} //"it is better to use the id of each of the expenses to avoid bugs"
-                onDate={expenses.date} 
-                onTitle={expenses.title}
-                onPrice={expenses.amount}
-            />  
-        )
-    })
+    
 
-    //If filtered data is empty a header with a message has to be return
-    if(filteredData.length === 0){
-        filteredData = <h1>No Expenses Recorded</h1>
-    }
 
     return (
         <div>
             <ExpensesFilter  onSelectedDate={selectedDate} />
 
             <ExpensesListCondition 
-            
-            onFilteredDataPerYear={filteredDataPerYear} 
-            onExpensesData={props.onExpensesData}
+                onFilteredDataPerYear={filteredDataPerYear} 
+                onExpensesData={props.onExpensesData}
             />
-
-            {/* {
-                filteredData
-            } */}
         
         </div>
     )
