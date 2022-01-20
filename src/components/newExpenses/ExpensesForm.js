50463f58f72d84cdf2419e75ebd6b018470a3cbc
Humberto <angeluccirincon@gmail.com>
2022-01-20 18:18:18 +0100

    add bool, create condition

--- src/components/newExpenses/ExpensesForm.js
+++ src/components/newExpenses/ExpensesForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 //Import css
 import "./expensesForm.css"
+import ShowForm from '../Form/ShowForm'
 
 function ExpensesForm(props) {
     //States
     const [newTitle, setNewTitle] = useState('')
     const [newAmount, setNewAmount] = useState('')
     const [newDate, setNewDate] = useState('')
+    const [showFormBool, setShowFormBool] = useState(false)
+
+    console.log(showFormBool)
 
     // const [newInput, setNewInput] = useState({
     //     enteredTitle: '',
@@ -47,45 +51,116 @@ function ExpensesForm(props) {
         setNewTitle('')
         setNewAmount('')
         setNewDate('')
+
+        setShowFormBool(false)
     }
+
+    //Hide form
+    const changeCondition = () => {
+        return setShowFormBool(false)
+    }
+
+    
     
     return (
         <div>
-            <form onSubmit={submitHandle}>
-                <div className="new-expense__controls">
-                    <div className="new-expense__control">
-                        <label>Title</label>
-                        <input 
-                        value={newTitle}
-                        type="text" 
-                        onChange={titleHandle} 
-                        />
+
+            {
+                showFormBool === false 
+                
+                ? 
+                
+                <ShowForm onSetShowFormBool={setShowFormBool} /> 
+                
+                : 
+                
+                <div>
+                <form onSubmit={submitHandle}>
+                    <div className="new-expense__controls">
+                        <div className="new-expense__control">
+                            <label>Title</label>
+                            <input 
+                            value={newTitle}
+                            type="text" 
+                            onChange={titleHandle} 
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Amount</label>
+                            <input 
+                            value={newAmount}
+                            type="number" 
+                            min="0.01" 
+                            step="0.01" 
+                            onChange={amountHandle}
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Date</label>
+                            <input 
+                            value={newDate}
+                            type="date" 
+                            min="2021-01-01" 
+                            max="2022-12-31"
+                            onChange={dateHandle} />
+                        </div>
+                        <div className="new-expense__actions">
+                            <button  
+                            type="submit"
+                            >
+                                Submit Expenses
+                            </button>
+                        </div>
                     </div>
-                    <div className="new-expense__control">
-                        <label>Amount</label>
-                        <input 
-                        value={newAmount}
-                        type="number" 
-                        min="0.01" 
-                        step="0.01" 
-                        onChange={amountHandle}
-                        />
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Date</label>
-                        <input 
-                        value={newDate}
-                        type="date" 
-                        min="2021-01-01" 
-                        max="2022-12-31"
-                        onChange={dateHandle} />
-                    </div>
-                    <div className="new-expense__actions">
-                        <button type="submit">Submit Expenses</button>
-                    </div>
+                </form>
+                        <div className="new-expense__actions">
+                            <button onClick={changeCondition}>Cancel</button>
+                        </div>
                 </div>
-            </form>
-        </div>
+            } 
+
+            {/* <ShowForm onSetShowFormBool={setShowFormBool} />
+            <div>
+                <form onSubmit={submitHandle}>
+                    <div className="new-expense__controls">
+                        <div className="new-expense__control">
+                            <label>Title</label>
+                            <input 
+                            value={newTitle}
+                            type="text" 
+                            onChange={titleHandle} 
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Amount</label>
+                            <input 
+                            value={newAmount}
+                            type="number" 
+                            min="0.01" 
+                            step="0.01" 
+                            onChange={amountHandle}
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Date</label>
+                            <input 
+                            value={newDate}
+                            type="date" 
+                            min="2021-01-01" 
+                            max="2022-12-31"
+                            onChange={dateHandle} />
+                        </div>
+                        <div className="new-expense__actions">
+                            <button type="submit">Submit Expenses</button>
+                        </div>
+                    </div>
+                </form>
+                        <div className="new-expense__actions">
+                            <button onClick={changeCondition}>Cancel</button>
+                        </div>
+                </div> */}
+                    
+            </div>
     )
 }
 
