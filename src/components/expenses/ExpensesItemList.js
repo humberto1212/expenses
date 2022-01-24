272901a78c4a13d8286b8804b93a45774606cb2f
Humberto <angeluccirincon@gmail.com>
2022-01-24 22:36:43 +0100

    add expensesChart

--- src/components/expenses/ExpensesItemList.js
+++ src/components/expenses/ExpensesItemList.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 //Components imports
-import ExpenseItem from './ExpenseItem'
 import ExpensesFilter from '../expensesFilter/ExpensesFilter'
 import ExpensesListCondition from '../expenses/ExpensesListCondition'
+import ExpensesChart from './ExpensesChart'
 
 function ExpensesItemList(props) {
     console.log(props.onExpensesData)
     //State
     const [filterYear, setFilterYear] = useState('')
+  
 
     const selectedDate = (selectedYear) => {
         setFilterYear(selectedYear)
@@ -24,12 +25,15 @@ function ExpensesItemList(props) {
         }
     } 
 
-    
+    let filteredExoensesChart = filteredDataPerYear(props.onExpensesData)
 
+    console.log("Filter for Chart",filteredExoensesChart)
 
     return (
         <div>
             <ExpensesFilter  onSelectedDate={selectedDate} />
+
+            <ExpensesChart expenses={filteredExoensesChart} />
 
             <ExpensesListCondition 
                 onFilteredDataPerYear={filteredDataPerYear} 
