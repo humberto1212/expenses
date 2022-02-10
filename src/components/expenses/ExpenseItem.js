1948e805ae4e72d6f730bb4268d9eeb6e7be733f
Humberto <angeluccirincon@gmail.com>
2022-02-10 21:39:40 +0100

    delete functions

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React from 'react'
 //Import CSS file
 import '../expenses/expenseItemStyle.css'
 //MUI imports
@@ -12,77 +12,9 @@ import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-    const [list, setList] = useState([
-        'Chicken Biriyani',
-        'Mutton Pulao',
-        'Paneer Masala',
-        'Tandoori Chicken',
-        'Dal Makhana',
-        'Malai Kofta',
-       ]);
-    
-
-    const draggingItem = useRef();
-    const dragOverItem = useRef();
-    //console.log("this is my props",props)
-
-    //Start drag action
-    const handleDragStart = (e, position) => {
-        draggingItem.current = position;
-        console.log("dragging start",e.target.innerHTML);
-    };
-    //Place drag action
-    const handleDragEnter = (e, position) => {
-        dragOverItem.current = position;
-        console.log("dragging over",e.target.innerHTML);
-       };
-    //Ends drag action
-    const handleDragEnd = (e) => {
-        const listCopy = [...props.onExpensesData];
-        //const listCopy = [...list];
-
-        const draggingItemContent = listCopy[draggingItem.current];
-      
-        listCopy.splice(draggingItem.current, 1);
-        listCopy.splice(dragOverItem.current, 0, draggingItemContent);
-         
-         draggingItem.current = null;
-         dragOverItem.current = null;
-
-
-         props.onSetExpensesData(listCopy);
-         //setList(listCopy)
-    };
-
-    console.log('new list',props.onExpensesData)
-    //console.log(list)
-
-  
     return (
-        <>
-         {/* {
-            list &&
-            list.map((item, index) => (
-             <h1 
-              onDragStart={(e) => handleDragStart(e, index)}
-              onDragEnter={(e) => handleDragEnter(e, index)}
-              onDragEnd={handleDragEnd}
-              key={index}
-              draggable
-               >
-                 {item}
-               </h1>
-              ))}  */}
-
-        <div 
-          key={props.index} 
-          raggable="true" 
-           onDragStart={(expense) => handleDragStart(expense, props.index)} 
-           onDragEnter={(expense) => handleDragEnter(expense, props.index)} 
-           onDragEnd={handleDragEnd} 
-           onDragOver={(e) => e.preventDefault()}
-        >
-
+        
+        <div>
             <Card sx={{ minWidth: 275 }}>
                 <h2>Expense Item</h2>
                 <CardContent className="card-content-flex"> 
@@ -99,7 +31,6 @@ function ExpenseItem(props) {
                 </CardContent>
             </Card>
         </div>
-        </>
     )
 }
 
